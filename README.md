0f4dc947458e2b92bf0d02aff94591ee9c788ad8
gparap <gparap@protonmail.com>
2021-04-19 17:56:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# firebase
+Firebase Projects
---
+++ blog/app/src/main/java/gparap/apps/blog/LoginActivity.java
@@ -0,0 +1,15 @@
+package gparap.apps.blog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().setTitle("Login");
+    }
+}
