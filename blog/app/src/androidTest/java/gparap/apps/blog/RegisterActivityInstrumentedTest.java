3eec27200d03324c1820d705242c625c569cd5d0
gparap <gparap@protonmail.com>
2021-06-23 16:27:52 +0200

    register user into firebase

--- blog/app/src/androidTest/java/gparap/apps/blog/RegisterActivityInstrumentedTest.java
+++ blog/app/src/androidTest/java/gparap/apps/blog/RegisterActivityInstrumentedTest.java
@@ -15,27 +15,42 @@
  */
 package gparap.apps.blog;
 
+import android.view.View;
+
 import androidx.test.core.app.ActivityScenario;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 
 public class RegisterActivityInstrumentedTest {
+    private View rootView;
 
     @Before
     public void setUp() {
-        ActivityScenario.launch(RegisterActivity.class);
+        //launch activity and retrieve the top-level window decor view
+        ActivityScenario<RegisterActivity> activityScenario = ActivityScenario.launch(RegisterActivity.class);
+        activityScenario.onActivity(activity -> rootView = activity.getWindow().getDecorView());
     }
 
     @Test
     public void isVisible_imageLogo() {
-onView(withId(R.id.imageViewRegisterLogo)).check(matches(isDisplayed()));
+        onView(withId(R.id.imageViewRegisterLogo)).check(matches(isDisplayed()));
     }
 
     @Test
@@ -66,5 +81,125 @@ onView(withId(R.id.imageViewRegisterLogo)).check(matches(isDisplayed()));
     @Test
     public void isInvisible_progressBar() {
         onView(withId(R.id.progressBarRegister)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void usernameIsEmpty_showErrorMessage() {
+        clearPreviousText(R.id.editTextRegisterUsername);
+        onView(withId(R.id.buttonRegister)).perform(click());
+        onView(withText(R.string.toast_empty_username))
+                .inRoot(withDecorView(not(is(rootView))))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void emailIsEmpty_showErrorMessage() {
+        //fill in username first
+        typeNewText(R.id.editTextRegisterUsername, "username");
+
+        clearPreviousText(R.id.editTextRegisterEmail);
+        onView(withId(R.id.buttonRegister)).perform(click());
+        onView(withText(R.string.toast_empty_email))
+                .inRoot(withDecorView(not(is(rootView))))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void passwordIsEmpty_showErrorMessage() {
+        //fill in username, email
+        typeNewText(R.id.editTextRegisterUsername, "username");
+        typeNewText(R.id.editTextRegisterEmail, "email");
+
+        clearPreviousText(R.id.editTextRegisterPassword);
+        onView(withId(R.id.buttonRegister)).perform(click());
+        onView(withText(R.string.toast_empty_password))
+                .inRoot(withDecorView(not(is(rootView))))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void passwordConfirmationIsEmpty_showErrorMessage() {
+        //fill in username, email and password
+        typeNewText(R.id.editTextRegisterUsername, "username");
+        typeNewText(R.id.editTextRegisterEmail, "email");
+        typeNewText(R.id.editTextRegisterPassword, "password");
+
+        clearPreviousText(R.id.editTextPasswordConfirm);
+        onView(withId(R.id.buttonRegister)).perform(click());
+        onView(withText(R.string.toast_empty_password_confirm))
+                .inRoot(withDecorView(not(is(rootView))))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void passwordAndConfirmationDoNotMatch_showErrorMessage() {
+        //fill in username, email
+        typeNewText(R.id.editTextRegisterUsername, "username");
+        typeNewText(R.id.editTextRegisterEmail, "email");
+
+        clearPreviousTextAndTypeNewText(R.id.editTextRegisterPassword, "password");
+        clearPreviousTextAndTypeNewText(R.id.editTextPasswordConfirm, "do no match");
+        onView(withId(R.id.buttonRegister)).perform(click());
+        onView(withText(R.string.toast_failed_password_confirm))
+                .inRoot(withDecorView(not(is(rootView))))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void userRegistration_success() throws InterruptedException {
+        //fill-in the input fields for a test user
+        typeNewText(R.id.editTextRegisterUsername, "username");
+        typeNewText(R.id.editTextRegisterEmail, "username@email.com");
+        typeNewText(R.id.editTextRegisterPassword, "123456");
+        typeNewText(R.id.editTextPasswordConfirm, "123456");
+
+        //register user and wait for firebase
+        onView(withId(R.id.buttonRegister)).perform(click());
+        Thread.sleep(1677);
+
+        onView(withId(R.id.layout_main_activity)).check(matches(isDisplayed()));
+
+        //delete test user and wait for firebase
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        assert firebaseUser != null;
+        firebaseUser.delete();
+        Thread.sleep(1677);
+    }
+
+    @Test
+    public void userRegistration_failed() throws InterruptedException {
+        //!!! test user (user1@com.com) exists in firebase
+        String existingUser = "user1@com.com";
+
+        //fill-in the input fields for a test user that matches existing user
+        typeNewText(R.id.editTextRegisterUsername, "username");
+        typeNewText(R.id.editTextRegisterEmail, existingUser);
+        typeNewText(R.id.editTextRegisterPassword, "123456");
+        typeNewText(R.id.editTextPasswordConfirm, "123456");
+
+        //register user and wait for firebase
+        onView(withId(R.id.buttonRegister)).perform(click());
+        Thread.sleep(1677);
+
+        onView(withText(R.string.toast_registration_failed))
+                .inRoot(withDecorView(not(is(rootView))))
+                .check(matches(isDisplayed()));
+    }
+
+    private void clearPreviousText(int viewId) {
+        onView(withId(viewId)).perform(clearText());
+        closeSoftKeyboard();
+    }
+
+    private void clearPreviousTextAndTypeNewText(int viewId, String text) {
+        onView(withId(viewId)).perform(clearText());
+        onView(withId(viewId)).perform(typeText(text));
+        closeSoftKeyboard();
+    }
+
+    private void typeNewText(int viewId, String text) {
+        onView(withId(viewId)).perform(typeText(text));
+        closeSoftKeyboard();
     }
 }
