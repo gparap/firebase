e2ddd8813816d7ee73e4243e83ec7ec28063cc0f
gparap <gparap@protonmail.com>
2021-07-06 17:24:28 +0200

    pick and crop image for user settings

--- blog/app/proguard-rules.pro
+++ blog/app/proguard-rules.pro
@@ -19,3 +19,6 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+# required by Android Image Cropper
+-keep class androidx.appcompat.widget.** { *; }
--- blog/app/src/main/java/gparap/apps/blog/ui/settings/UserSettingsActivity.java
+++ blog/app/src/main/java/gparap/apps/blog/ui/settings/UserSettingsActivity.java
@@ -1,8 +1,6 @@
 package gparap.apps.blog.ui.settings;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
@@ -12,9 +10,14 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -24,13 +27,14 @@ import gparap.apps.blog.R;
 import gparap.apps.blog.model.BlogUserModel;
 import gparap.apps.blog.utils.FirebaseUtils;
 
-@SuppressWarnings({"FieldCanBeLocal", "unused"})
+@SuppressWarnings({"FieldCanBeLocal", "unused", "deprecation", "ConstantConditions"})
 public class UserSettingsActivity extends AppCompatActivity {
     private ImageButton buttonSetUserImage, buttonChangeUsername, buttonChangeEmail, buttonChangePassword;
     private EditText username, email, password, passwordConfirm;
     private Button buttonUpdate;
     private ProgressBar progressBar;
     private BlogUserModel user;
+    private final int ACTION_GET_CONTENT_RESULT_CODE = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,14 @@ public class UserSettingsActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle("User Settings");
         getWidgets();
         setListenersToMakeInputWidgetsEditable();
+
+        //pick an image from user device gallery
+        buttonSetUserImage.setOnClickListener(v -> {
+            Intent intentGetContent = new Intent();
+            intentGetContent.setType("image/*");
+            intentGetContent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intentGetContent, ACTION_GET_CONTENT_RESULT_CODE);
+        });
 
         //get user from database
         String userId = FirebaseUtils.getInstance().getUser().getUid();
@@ -62,6 +74,34 @@ public class UserSettingsActivity extends AppCompatActivity {
                 Log.e("DatabaseError", error.getDetails());
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //start cropping activity
+        if (requestCode == ACTION_GET_CONTENT_RESULT_CODE) {
+            CropImage.activity(data.getData())
+                    .start(this);
+        }
+
+        //get crop result
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                //set user image
+                buttonSetUserImage.setImageURI(result.getUri());
+                buttonSetUserImage.setBackground(null);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                try {
+                    throw error;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private void displayUserSettings() {
