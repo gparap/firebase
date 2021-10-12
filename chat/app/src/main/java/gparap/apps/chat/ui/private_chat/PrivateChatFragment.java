ef505f337ded89d3a65b80d8c9ef745d468b9e4a
gparap <gparap@protonmail.com>
2021-10-12 14:08:38 +0200

    Create tabbed layout for private and public chat

---
+++ chat/app/src/main/java/gparap/apps/chat/ui/private_chat/PrivateChatFragment.java
@@ -0,0 +1,38 @@
+package gparap.apps.chat.ui.private_chat;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gparap.apps.chat.R;
+
+public class PrivateChatFragment extends Fragment {
+
+    private PrivateChatViewModel mViewModel;
+
+    public static PrivateChatFragment newInstance() {
+        return new PrivateChatFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_private_chat, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PrivateChatViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ chat/app/src/main/java/gparap/apps/chat/ui/private_chat/PrivateChatViewModel.java
@@ -0,0 +1,7 @@
+package gparap.apps.chat.ui.private_chat;
+
+import androidx.lifecycle.ViewModel;
+
+public class PrivateChatViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
