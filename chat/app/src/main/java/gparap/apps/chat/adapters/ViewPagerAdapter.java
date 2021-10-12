62ecc3ef4ee5bc2545f10b4557cfcfd0cfd7665a
gparap <gparap@protonmail.com>
2021-10-12 14:08:38 +0200

    Create tabbed layout for private and public chat

---
+++ chat/app/src/main/java/gparap/apps/chat/adapters/ViewPagerAdapter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.chat.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import gparap.apps.chat.ui.private_chat.PrivateChatFragment;
+import gparap.apps.chat.ui.public_chat.PublicChatFragment;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private static final int FRAGMENTS_COUNT = 2;
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return PrivateChatFragment.newInstance();
+            case 1:
+                return PublicChatFragment.newInstance();
+            default:
+                throw new IllegalStateException("DEBUG: this shouldn't have happened..");
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return FRAGMENTS_COUNT;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Private Chat";
+            case 1:
+                return "Public Chat";
+            default:
+                throw new IllegalStateException("DEBUG: this shouldn't have happened..");
+        }
+    }
+}
