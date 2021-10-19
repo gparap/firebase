9b962226d29b6a6db74877f69a176dc0791420ba
gparap <gparap@protonmail.com>
2021-10-19 14:06:36 +0200

    Display user profile

--- chat/app/src/main/java/gparap/apps/chat/ui/user_profile/UserProfileActivity.java
+++ chat/app/src/main/java/gparap/apps/chat/ui/user_profile/UserProfileActivity.java
@@ -3,20 +3,25 @@ package gparap.apps.chat.ui.user_profile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import gparap.apps.chat.R;
 
 public class UserProfileActivity extends AppCompatActivity {
-    private ImageView user_profileImage, editDisplayName, editEmail, editPassword;
+    private ImageView userProfileImage, editDisplayName, editEmail, editPassword;
     private EditText displayName, email, password, confirmPassword;
     private Button buttonUpdate;
     private ProgressBar progressUpdate;
+    private boolean isDisplayNameEditable, isEmailEditable, isPasswordEditable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,48 @@ public class UserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile);
         setupToolbar();
         getWidgets();
+        setListenersToMakeInputWidgetsEditable();
+        displayUserProfileDetails();
+        //TODO: update user profile
+        //TODO: update password
+    }
+
+    private void displayUserProfileDetails() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            displayName.setText(firebaseUser.getDisplayName());
+            email.setText(firebaseUser.getEmail());
+        }
+    }
+
+    private void setListenersToMakeInputWidgetsEditable() {
+        //make change username editable
+        editDisplayName.setOnClickListener(v -> {
+                    displayName.setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+                    displayName.setEnabled(true);
+                    isDisplayNameEditable = true;
+                }
+        );
+
+        //make change email editable
+        editEmail.setOnClickListener(v -> {
+                    email.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                    email.setEnabled(true);
+                    isEmailEditable = true;
+                }
+        );
+
+        //make change password editable and show password confirmation
+        editPassword.setOnClickListener(v -> {
+            password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            password.setEnabled(true);
+            confirmPassword.setVisibility(View.VISIBLE);
+            isPasswordEditable = true;
+        });
     }
 
     private void getWidgets() {
-        user_profileImage = findViewById(R.id.image_view_user_profile);
+        userProfileImage = findViewById(R.id.image_view_user_profile);
         editDisplayName = findViewById(R.id.image_view_edit_user_display_name);
         editEmail = findViewById(R.id.image_view_edit_user_email);
         editPassword = findViewById(R.id.image_view_edit_password);
