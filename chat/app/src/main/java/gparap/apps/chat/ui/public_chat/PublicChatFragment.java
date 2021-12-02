904733dd55c96f7aadc7b14b8952e2339b9790df
gparap <gparap@protonmail.com>
2021-12-02 17:50:28 +0200

    Stores a user's public message on the database

--- chat/app/src/main/java/gparap/apps/chat/ui/public_chat/PublicChatFragment.java
+++ chat/app/src/main/java/gparap/apps/chat/ui/public_chat/PublicChatFragment.java
@@ -1,22 +1,26 @@
 package gparap.apps.chat.ui.public_chat;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.lifecycle.ViewModelProvider;
 
 import gparap.apps.chat.R;
+import gparap.apps.chat.data.MessageModel;
+import gparap.apps.chat.data.UserModel;
+import gparap.apps.chat.utils.AppConstants;
 
 public class PublicChatFragment extends Fragment {
-
-    private PublicChatViewModel mViewModel;
+    private ViewGroup container;
+    private PublicChatViewModel viewModel;
+    public UserModel signedInUser;
 
     public static PublicChatFragment newInstance() {
         return new PublicChatFragment();
@@ -25,14 +29,42 @@ public class PublicChatFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        //get signed-in user (if exists)
+        if (getActivity() != null && getActivity().getIntent() != null) {
+            signedInUser = getActivity().getIntent().getParcelableExtra(AppConstants.SIGNED_IN_USER);
+        }
+
+        //get this Fragment's container
+        this.container = container;
+
+        //inflate chat list's layout
         return inflater.inflate(R.layout.fragment_public_chat, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(PublicChatViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
+        //create the ViewModel for this Fragment
+        viewModel = new ViewModelProvider(this).get(PublicChatViewModel.class);
+
+        //get fragment widgets
+        ImageView buttonSendMsg = container.findViewById(R.id.image_view_public_chat_send_message);
+        EditText msgText = container.findViewById(R.id.edit_text_public_chat_message);
+
+        //send public message
+        buttonSendMsg.setOnClickListener(view -> {
+            if (msgText.getText().toString().isEmpty()) { return; }
+
+            //create the message
+            MessageModel message = new MessageModel();
+            message.setSender(signedInUser.getDisplayName());
+            message.setMessage(msgText.getText().toString().trim());
+
+            if (!msgText.getText().toString().isEmpty()) {
+                viewModel.sendPublicMessage(message);
+            }
+        });
+    }
 }
--- chat/app/src/main/java/gparap/apps/chat/ui/public_chat/PublicChatViewModel.java
+++ chat/app/src/main/java/gparap/apps/chat/ui/public_chat/PublicChatViewModel.java
@@ -2,6 +2,17 @@ package gparap.apps.chat.ui.public_chat;
 
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.database.FirebaseDatabase;
+
+import gparap.apps.chat.data.MessageModel;
+import gparap.apps.chat.utils.AppConstants;
+
 public class PublicChatViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    /* Stores a user's public message on the database */
+    public void sendPublicMessage(MessageModel message) {
+        FirebaseDatabase.getInstance(AppConstants.DATABASE_URL)
+                .getReference(AppConstants.DATABASE_PATH_PUBLIC_MESSAGES)
+                .push().setValue(message);
+    }
 }
