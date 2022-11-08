2d049421d6536ffa6c70d4336b3b41dc745e040e
gparap <gparap@protonmail.com>
2022-11-08 13:37:32 +0200

    Handle READ_EXTERNAL_STORAGE permission for android SDK >= 23

--- player-music/app/src/main/java/gparap/apps/player_music/MainActivity.java
+++ player-music/app/src/main/java/gparap/apps/player_music/MainActivity.java
@@ -1,8 +1,16 @@
 package gparap.apps.player_music;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+import static gparap.apps.player_music.utils.AppConstants.REQUEST_CODE_READ_EXTERNAL_STORAGE;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.FirebaseApp;
 
@@ -12,6 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializes the default FirebaseApp instance
         FirebaseApp.initializeApp(this);
+
+        //determine whether the app have been granted the READ_EXTERNAL_STORAGE permission
+        if (ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            //TODO(SDK >= 23 && < 33))
+
+        } else {
+            //ask for READ_EXTERNAL_STORAGE permission (SDK >= 23 && < 33)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+                requestPermissions(new String[]{READ_EXTERNAL_STORAGE}, REQUEST_CODE_READ_EXTERNAL_STORAGE);
+            }
+
+            //TODO: MANAGE_EXTERNAL_STORAGE (SDK >= 33)
+            //do not ask for READ_EXTERNAL_STORAGE permission (SDK >= 33)
+            else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                //TODO(SDK >= 33)
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_READ_EXTERNAL_STORAGE) {
+            //TODO (SDK >= 21)
+        }
     }
 }
---
+++ player-music/app/src/main/java/gparap/apps/player_music/utils/AppConstants.java
@@ -0,0 +1,5 @@
+package gparap.apps.player_music.utils;
+
+public class AppConstants {
+    public static final int REQUEST_CODE_READ_EXTERNAL_STORAGE = 999;
+}
