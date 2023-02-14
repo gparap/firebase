819ca82ab558c95322e3e3df28129edc2dd6382d
gparap <gparap@protonmail.com>
2023-02-14 12:52:34 +0200

    Set the device wallpaper

--- wallpaper/app/src/main/java/gparap/apps/wallpaper/adapters/WallpaperAdapter.java
+++ wallpaper/app/src/main/java/gparap/apps/wallpaper/adapters/WallpaperAdapter.java
@@ -15,17 +15,25 @@
  */
 package gparap.apps.wallpaper.adapters;
 
+import android.app.WallpaperManager;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import gparap.apps.wallpaper.R;
@@ -62,6 +70,41 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.Wall
                 .load(wallpapers.get(position).getUrl())
                 .fit()
                 .into(holder.image);
+
+        //display wallpaper name
+        holder.itemView.setOnClickListener(v ->
+                Toast.makeText(context, wallpapers.get(position).getName(), Toast.LENGTH_SHORT).show()
+        );
+
+        //get dimensions of the device display
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        AppCompatActivity activity = (AppCompatActivity) context;
+        activity.getWindow().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int deviceWidth = displayMetrics.widthPixels;
+        int deviceHeight = displayMetrics.heightPixels;
+
+        //set the device wallpaper
+        holder.fabSetWallpaper.setOnClickListener(v -> {
+            //create bitmap from wallpaper image and scale it to device dimensions
+            Bitmap bitmap = Bitmap.createScaledBitmap(
+                    ((BitmapDrawable)holder.image.getDrawable()).getBitmap(),
+                    deviceWidth, deviceHeight, true
+            );
+
+            //set the wallpaper
+            WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
+            try {
+                wallpaperManager.setBitmap(bitmap);
+                Toast.makeText(context,
+                    context.getResources().getString(R.string.toast_wallpaper_set),
+                    Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                Toast.makeText(context,
+                        context.getResources().getString(R.string.toast_wallpaper_set),
+                        Toast.LENGTH_SHORT).show();
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
@@ -71,10 +114,12 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.Wall
 
     public static class WallpaperViewHolder extends RecyclerView.ViewHolder {
         private final ImageView image;
+        private final FloatingActionButton fabSetWallpaper;
 
         public WallpaperViewHolder(@NonNull View itemView) {
             super(itemView);
             image = itemView.findViewById(R.id.imageViewWallpaper);
+            fabSetWallpaper = itemView.findViewById(R.id.fabSetWallpaper);
         }
     }
 }
