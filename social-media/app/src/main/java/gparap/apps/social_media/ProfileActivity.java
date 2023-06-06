734367401b7e1c70d5b5fd1d9d2700630f26108f
gparap <gparap@protonmail.com>
2023-06-06 16:01:06 +0300

    Add user profile image to database storage

--- social-media/app/src/main/java/gparap/apps/social_media/ProfileActivity.java
+++ social-media/app/src/main/java/gparap/apps/social_media/ProfileActivity.java
@@ -15,22 +15,35 @@
  */
 package gparap.apps.social_media;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.Objects;
+import java.util.Random;
 
 import gparap.apps.social_media.data.UserModel;
 
@@ -41,6 +54,9 @@ public class ProfileActivity extends AppCompatActivity {
             editTextProfilePassword, editTextProfilePasswordConfirm;
     private Button buttonProfileUpdate;
     private ProgressBar progressBarProfile;
+    private final int GET_CONTENT_REQUEST_CODE = 999;
+    UserModel dbUser;
+    Uri userProfileImageUriData = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,13 +78,74 @@ public class ProfileActivity extends AppCompatActivity {
             Task<DataSnapshot> snapshotTask = userRef.get();
             snapshotTask.addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
-                    UserModel dbUser = task.getResult().getValue(UserModel.class);
+                    dbUser = task.getResult().getValue(UserModel.class);
                     if (dbUser != null) {
                         editTextProfileMobile.setText(dbUser.getPhone());
                         editTextProfilePassword.setText(dbUser.getPassword());
+
+                        //display user profile image
+                        if (!dbUser.getImageUrl().isEmpty()) {
+                            Uri uri = Uri.parse(dbUser.getImageUrl());
+                            Picasso.get().load(uri).into(imageButtonUserProfile);
+                        }
                     }
                 }
             });
+        }
+
+        //set user profile image
+        imageButtonUserProfile = findViewById(R.id.imageButtonUserProfile);
+        imageButtonUserProfile.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intent, GET_CONTENT_REQUEST_CODE);
+        });
+
+        //update user profile
+        buttonProfileUpdate = findViewById(R.id.buttonProfileUpdate);
+        buttonProfileUpdate.setOnClickListener(v -> {
+            //add image to database storage
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference("social_media_app").child(editTextProfileUsername.getText().toString().trim());
+            StorageReference childRef = storageRef.child(String.valueOf(Math.abs((new Random()).nextLong())));
+            UploadTask uploadTask = childRef.putFile(userProfileImageUriData);
+            uploadTask.continueWithTask(task -> {
+                if (!task.isSuccessful()) {
+                    throw Objects.requireNonNull(task.getException());
+                }
+
+                //continue with the task to get the download URL
+                return childRef.getDownloadUrl();
+            }).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Uri downloadUrl = task.getResult();
+
+                    //update user reference in database
+                    dbUser.setImageUrl(downloadUrl.toString());
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference usersRef = database.getReference("social_media_app").child("users").child(dbUser.getUsername());
+                    usersRef.setValue(dbUser);
+                }
+            });
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //display selected avatar and keep uri data
+        if (requestCode == GET_CONTENT_REQUEST_CODE) {
+            Bitmap bitmap;
+            try {
+                if (data != null) {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                    imageButtonUserProfile.setImageBitmap(bitmap);
+                    userProfileImageUriData = data.getData();
+                }
+            } catch (Exception ignored) {
+            }
         }
     }
 
