a0edb867d5ad06ff2d71eaf08e2d62b73af6a62b
gparap <gparap@protonmail.com>
2023-10-31 11:01:03 +0200

    Create functionality for deleting posts

---
+++ social-media/app/src/androidTest/java/gparap/apps/social_media/PostActivityInstrumentedTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2023 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.social_media;
+
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Objects;
+
+import gparap.apps.social_media.auth.LoginActivity;
+
+/**
+ * @noinspection FieldCanBeLocal
+ */
+public class PostActivityInstrumentedTest {
+    private ActivityScenario<LoginActivity> loginActivityActivityScenario;
+    private ActivityScenario<MainActivity> mainActivityScenario;
+
+    //!!! Use this default test user credentials, they don't change
+    final private String testUser_name = "gp";
+    final private String testUser_email = "gp@dot.com";
+    final private String testUser_password = "123123";
+
+    Long timestamp = System.currentTimeMillis();
+    private final String postTitle = String.valueOf(timestamp);
+    private final String postDetails = "post details";
+    private final String postBy = "posted by\u00A0";
+
+    int count = 0;
+
+    @Before
+    public void setUp() throws InterruptedException {
+        //sign-out existing user (if any)
+        try {
+            FirebaseAuth.getInstance().signOut();
+        } catch (Exception ignored) {
+        }
+
+        //sign-in with test user
+        loginActivityActivityScenario = ActivityScenario.launch(LoginActivity.class);
+        signInUser();
+
+        //add a test post and finish current scenario
+        addNewPost(postTitle, postDetails);
+        loginActivityActivityScenario.close();
+
+        //launch the main scenario and open the most recent post
+        mainActivityScenario = ActivityScenario.launch(MainActivity.class);
+        mainActivityScenario.onActivity(activity -> {
+            RecyclerView recyclerView = activity.findViewById(R.id.recycler_view_posts);
+            count = Objects.requireNonNull(recyclerView.getAdapter()).getItemCount();
+        });
+        Thread.sleep(4667); //!!!do NOT remove this
+        onView(withId(R.id.recycler_view_posts)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(count - 1, click()));
+    }
+
+    @Test
+    public void areVisible_allWidgetsExceptImageView() {
+        onView(withId(R.id.textViewPostTitle)).check(matches(isDisplayed()));
+        onView(withId(R.id.textViewPostDetails)).check(matches(isDisplayed()));
+        onView(withId(R.id.textViewPostCreator)).check(matches(isDisplayed()));
+        onView(withId(R.id.imageButton_deletePost)).check(matches(isDisplayed()));
+        onView(withId(R.id.imageButton_editPost)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void openPost_isPostCorrect() {
+        onView(withId(R.id.textViewPostTitle)).check(matches(withText(postTitle)));
+        onView(withId(R.id.textViewPostDetails)).check(matches(withText(postDetails)));
+        onView(withId(R.id.textViewPostCreator)).check(matches(withText(postBy + testUser_name)));
+    }
+
+    @Test
+    public void deletePost_isPostDeleted() throws InterruptedException {
+        onView(withId(R.id.imageButton_deletePost)).perform(click());
+        onView(withText(R.string.text_ok)).perform(click());
+        Thread.sleep(Toast.LENGTH_SHORT);
+        onView(withText(postTitle)).check(matches(not(isDisplayed())));
+    }
+
+    private void signInUser() throws InterruptedException {
+        onView(withId(R.id.editTextLoginEmail)).perform(typeText(testUser_email));
+        closeSoftKeyboard();
+        onView(withId(R.id.editTextLoginPassword)).perform(typeText(testUser_password));
+        closeSoftKeyboard();
+        onView(withId(R.id.buttonLogin)).perform(click());
+        Thread.sleep(1667); //wait for firebase..
+    }
+
+    /**
+     * @noinspection SameParameterValue
+     */
+    private void addNewPost(String title, String details) throws InterruptedException {
+        onView(withId(R.id.fab_add_post_main)).perform(click());
+        onView(withId(R.id.textViewPostTitle_thumbnail)).perform(typeText(title));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.textViewPostDetails_thumbnail)).perform(typeText(details));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.buttonSavePost)).perform(click());
+        Thread.sleep(667);
+    }
+}
--- social-media/app/src/main/java/gparap/apps/social_media/PostActivity.java
+++ social-media/app/src/main/java/gparap/apps/social_media/PostActivity.java
@@ -15,16 +15,23 @@
  */
 package gparap.apps.social_media;
 
+import static gparap.apps.social_media.utils.AppConstants.DATABASE_REFERENCE;
+import static gparap.apps.social_media.utils.AppConstants.DATABASE_REFERENCE_POSTS;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import java.util.Objects;
@@ -33,6 +40,7 @@ import gparap.apps.social_media.data.PostModel;
 import gparap.apps.social_media.utils.AppConstants;
 
 public class PostActivity extends AppCompatActivity {
+    private PostModel post = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +48,6 @@ public class PostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post);
 
         //get post extras from intent
-        PostModel post = null;
         String username = "";
         if (getIntent() != null) {
             post = new PostModel(getIntent().getStringExtra(AppConstants.POST_ID),
@@ -53,20 +60,46 @@ public class PostActivity extends AppCompatActivity {
 
         //display post texts
         assert post != null;
-        ((TextView)findViewById(R.id.textViewPostTitle)).setText(post.getTitle());
-        ((TextView)findViewById(R.id.textViewPostDetails)).setText(post.getDetails());
-        ((TextView)findViewById(R.id.textViewPostCreator)).setText(
+        ((TextView) findViewById(R.id.textViewPostTitle)).setText(post.getTitle());
+        ((TextView) findViewById(R.id.textViewPostDetails)).setText(post.getDetails());
+        ((TextView) findViewById(R.id.textViewPostCreator)).setText(
                 String.format("%s%s", getString(R.string.text_posted_by), username)
         );
 
         //display post image
-        Picasso.get().load(post.getImageUrl()).into(((ImageView)findViewById(R.id.imageViewPost)));
+        Picasso.get().load(post.getImageUrl()).into(((ImageView) findViewById(R.id.imageViewPost)));
 
         //if the user that views the post is also its creator, show the related buttons
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser != null && Objects.equals(currentUser.getDisplayName(), username)) {
+            //delete post
             ImageButton buttonDeletePost = findViewById(R.id.imageButton_deletePost);
             buttonDeletePost.setVisibility(View.VISIBLE);
+            buttonDeletePost.setOnClickListener(v -> {
+                //display a confirmation dialog
+                new AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.text_post_deleted_alert_dialog))
+                        .setPositiveButton(getString(R.string.text_ok), (dialog, which) ->
+                                FirebaseDatabase.getInstance().getReference().getDatabase()
+                                        .getReference(DATABASE_REFERENCE)
+                                        .child(DATABASE_REFERENCE_POSTS).child(post.getId())
+                                        .removeValue().addOnCompleteListener(task -> {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(this, getString(R.string.text_post_deleted), Toast.LENGTH_SHORT).show();
+
+                                                //close this activity and return to application posts
+                                                this.startActivity(new Intent(this, MainActivity.class));
+                                                this.finish();
+
+                                            } else if (task.getException() != null) {
+                                                Toast.makeText(this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        }))
+                        .setNegativeButton(getString(R.string.text_cancel), (dialog, which) -> dialog.dismiss())
+                        .create().show();
+            });
+
+            //edit post
             ImageButton buttonEditPost = findViewById(R.id.imageButton_editPost);
             buttonEditPost.setVisibility(View.VISIBLE);
         }
